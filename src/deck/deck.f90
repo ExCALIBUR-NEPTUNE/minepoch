e4dc99da586d435c896c36cbbc95306705c600fa
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2021-02-10 14:30:13 +0000

    Add deck reading code

--- src/deck/deck.f90
+++ src/deck/deck.f90
@@ -8,29 +8,91 @@ CONTAINS
 
   SUBROUTINE read_deck
 
-    INTEGER :: nlines, ierr
-    INTEGER :: errcode
+    INTEGER :: ierr, i, errcode
+    CHARACTER(LEN=1024), ALLOCATABLE, SAVE :: lines(:)
     LOGICAL, SAVE :: first = .TRUE.
+    INTEGER, SAVE :: nlines = 0
 
     NAMELIST/control/ problem
 
     IF (first) THEN
-      nlines = 0
+      ! Set the default problem here
+      problem = 'two_stream'
+
       ! Check for input deck existing.
-      ! If exists, read into buffer, otherwise abort
+      ! If exists, read into buffer, otherwise print warning
       IF (rank == 0) THEN
         OPEN(UNIT=lu, STATUS='OLD', FILE=TRIM(data_dir) // '/input.deck', &
              IOSTAT=ierr)
 
         IF (ierr /= 0) THEN
-          PRINT *, 'Failed to open file: ' // TRIM(data_dir) // '/input.deck'
-          CALL MPI_ABORT(MPI_COMM_WORLD, c_err_io, errcode)
+          PRINT *, 'WARNING: Failed to open file: ' // TRIM(data_dir) // '/input.deck'
+        ELSE
+          ! Count lines in deck
+          DO
+            READ(lu, FMT=*, IOSTAT=ierr)
+            IF (ierr /= 0) EXIT
+            nlines = nlines + 1
+          END DO
+
+          ! Allocate the buffer
+          ALLOCATE(lines(nlines))
+
+          ! Rewind file, read into buffer
+          REWIND(lu)
+          DO i = 1, nlines
+            READ(lu, FMT='(A)', IOSTAT=ierr) lines(i)
+            IF (ierr /= 0) THEN
+              PRINT*, 'Error processing input file: ' // TRIM(data_dir) // 'input.deck'
+              CALL MPI_ABORT(MPI_COMM_WORLD, c_err_io, errcode)
+            END IF
+          END DO
+
+          CLOSE(lu)
         END IF
+      END IF
+
+      ! Broadcast buffer to other procs
+      IF (nproc > 1) THEN
+
+        CALL MPI_BCAST(nlines, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, errcode)
+
+        IF (nlines > 0) THEN
+          IF (rank > 0) THEN
+            ALLOCATE(lines(nlines))
+          END IF
+
+          CALL MPI_BCAST(lines, nlines*LEN(lines(1)), MPI_CHARACTER, 0, &
+              MPI_COMM_WORLD, errcode)
+        END IF
+      END IF
+
+      ! No buffer to read
+      IF (nlines == 0) THEN
+        first = .FALSE.
+        RETURN
+      END IF
+
+      ! Read deck
+      READ(lines, NML=control, IOSTAT=ierr)
+      IF (ierr /= 0) THEN
+        IF (rank == 0) PRINT *, 'Error reading namelist'
+        CALL MPI_ABORT(MPI_COMM_WORLD, c_err_io, errcode)
       END IF
 
       first = .FALSE.
     ELSE
+      ! No buffer to read
+      IF (nlines == 0) THEN
+        RETURN
+      END IF
 
+      ! Read deck
+      READ(lines, NML=control, IOSTAT=ierr)
+      IF (ierr /= 0) THEN
+        IF (rank == 0) PRINT *, 'Error reading namelist'
+        CALL MPI_ABORT(MPI_COMM_WORLD, c_err_io, errcode)
+      END IF
     END IF
 
   END SUBROUTINE read_deck

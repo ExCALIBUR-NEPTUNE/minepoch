bfe35d47ea20df2feae9f68bcbcc4549f9720bdd
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2021-04-28 20:27:33 +0100

    Add ability to specify tolerances for test

--- minepoch_py/test.py
+++ minepoch_py/test.py
@@ -1,27 +1,58 @@
 #!/usr/bin/env python3
-import numpy as np
 import sys
+import argparse
+import numpy as np
 from minepoch_py.energy import energy_check
 from minepoch_py.two_stream import two_stream_analysis
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="""Basic CI testing script for minepoch
+        """,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    group = parser.add_argument_group("Tolerances")
+
+    group.add_argument(
+        "--energy_conservation",
+        default=1e-4,
+        type=float,
+        help="Allowed fractional error in energy conservation",
+        nargs="?",
+    )
+
+    group.add_argument(
+        "--growth_rate",
+        default=0.05,
+        type=float,
+        help="Allowed fractional error growth rate",
+        nargs="?",
+    )
+
+    return parser.parse_args()
+
+
 def run_tests(plot=True, savefig=True):
 
-    energy_error = energy_check(tolerance=1e-4, plot=plot, savefig=savefig)
+    options = parse_arguments()
 
-    rate, _ = two_stream_analysis(check_growth=True, plot=plot, savefig=savefig)
+    energy_error = energy_check(tolerance=options.energy_conservation,
+                                plot=plot, savefig=savefig)
 
-    # Allow 5% error on growth rate
-    rate_tolerance = 0.05
+    rate, _ = two_stream_analysis(check_growth=True, plot=plot,
+                                  savefig=savefig)
+
     # Analytic rate
     rate_analytic = 0.7
     rate_error = np.abs(rate - rate_analytic) / rate_analytic
 
     # Return 1 or 0, depending on error > tolerance
-    if (energy_error > 0 or rate_error > rate_tolerance):
+    if (energy_error > 0 or rate_error > options.growth_rate):
         if energy_error > 0:
             print('Energy conservation check failed!')
-        if rate_error > rate_tolerance:
+        if rate_error > options.growth_rate:
             print('Two stream growth check failed!')
             print('Observed growth rate = %.4E (expected %.2F)'
                   % (rate, rate_analytic))

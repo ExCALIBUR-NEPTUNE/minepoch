eee3639c1c07928a117e29f94b18f97457197b5b
Tom Goffrey <t.goffrey.1@warwick.ac.uk>
2021-07-20 14:38:01 +0100

    Add comments to particle_init module

--- src/user_interaction/particle_init.F90
+++ src/user_interaction/particle_init.F90
@@ -7,6 +7,21 @@ MODULE particle_init
 
 CONTAINS
 
+  !****************************************************************************
+  !> \brief Uniformly load particles in a given region
+  !>
+  !> Subroutine for calculating initial particle positions.
+  !> Positions sampled from a uniform region of space defined by
+  !> x0 +/- dx/2, y0 +/- dy/2 z0 +/- dz/2
+  !>
+  !> @param[inout] part Particle to initialisation position of
+  !> @param[in] x x-coordinate of central point of region to load particle in
+  !> @param[in] y y-coordinate of central point of region to load particle in
+  !> @param[in] z z-coordinate of central point of region to load particle in
+  !> @param[in] dx width of region to load particle in, in x-direction
+  !> @param[in] dy width of region to load particle in, in y-direction
+  !> @param[in] dz width of region to load particle in, in z-direction
+  !****************************************************************************
   SUBROUTINE init_particle_position(part, x, y, z, dx, dy, dz)
 
     TYPE(particle), POINTER, INTENT(INOUT) :: part
@@ -19,6 +34,18 @@ CONTAINS
   END SUBROUTINE init_particle_position
 
 
+
+  !****************************************************************************
+  !> \brief Calculate particle momentum from temperature..
+  !>
+  !> Function to initialise particle momentum components, assuming a Maxwellian
+  !> distribution function
+  !>
+  !> @param[in] mass Mass of particle (kg)
+  !> @param[in] temperature Temperature of particle (K)
+  !> @param[in] drift Drift (mean) momentum of distribution (kg m/s)
+  !> @return Component of momentum
+  !****************************************************************************
   FUNCTION momentum_from_temperature(mass, temperature, drift)
 
     REAL(num), INTENT(IN) :: mass, temperature, drift
